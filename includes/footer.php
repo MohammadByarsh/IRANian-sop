dec9cb4a2f3db45d67da327d29caf6273bcacf7c
MohammadByarsh <wolfof77game@gmail.com>
2025-05-09 21:04:07 +0100

    iranian sop

---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+</section>
+</section>
+</section>
+<footer class="divTable">
+  <div class="divTableRow">
+    <span class='divTableCell' id='DateTime'></span>
+  </div>
+  <section class="divTableRow">
+    <article class="divTableCell" id="footer"><a href="http://al1almasi.ir" target="_blank" rel="noopener noreferrer">حق کپی رایت</a> &copy;</span></article>
+  </section>
+</footer>
+</div>
+</div>
+</div>
+
+
+</body>
+
+</html>
